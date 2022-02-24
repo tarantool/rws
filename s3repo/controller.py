ea441f08b1736b2a0829bb5e8047b373febdc5d3
Leonid Vasiliev <lvasiliev@tarantool.org>
2022-02-24 11:26:01 +0300

    rws: add the ability to upload a package to several repositories
    Closes #48

--- s3repo/controller.py
+++ s3repo/controller.py
@@ -1,7 +1,9 @@
 """Controllers for working with S3."""
 
+import copy
 import logging
 import os
+import re
 
 from flask import jsonify
 from flask import request
@@ -17,8 +19,9 @@ from s3repo.repoinfo import RepoAnnotation
 class S3Controller(MethodView):
     """Controller for working with S3 according to the REST model."""
 
-    def __init__(self, model):
+    def __init__(self, model, anchors):
         self.model = model
+        self.anchors = anchors
 
     @staticmethod
     def check_filename(filename):
@@ -35,6 +38,40 @@ class S3Controller(MethodView):
         response = jsonify({'message': message})
         response.status_code = status
         return response
+
+    def _generate_repo_annotations(self, path):
+        """Generates a list of repository annotations according to the path."""
+        repo_annotations = []
+        repo_anchors = {}
+
+        # Find the anchors in the URI.
+        path_list = path.split('/')
+        for i in range(0, len(path_list)):
+            if path_list[i] in self.anchors:
+                repo_anchors[i] = self.anchors[path_list[i]]
+
+        # If no anchors are found, we use the passed URI to form the RepoAnnotation.
+        if len(repo_anchors) == 0:
+            repo_annotations.append(RepoAnnotation(path, self.model.get_supported_repos()))
+            return repo_annotations
+
+        # Generate a list of URIs according to the present anchors.
+        generated_path_lists = [path_list.copy()]
+        for anchor_position, anchor_value_list in repo_anchors.items():
+            tmp_generated_path_lists =[]
+            for anchor_value in anchor_value_list:
+                for generated_path_list in generated_path_lists:
+                    tmp_generated_path_list = copy.deepcopy(generated_path_list)
+                    tmp_generated_path_list[anchor_position] = anchor_value
+                    tmp_generated_path_lists.append(tmp_generated_path_list)
+            generated_path_lists = tmp_generated_path_lists
+
+        # Generate the RepoAnnotations list according to the generated URI list.
+        for generated_path_list in generated_path_lists:
+            generated_path = os.path.normpath('/'.join(generated_path_list))
+            repo_annotations.append(RepoAnnotation(generated_path, self.model.get_supported_repos()))
+
+        return repo_annotations
 
     @auth_provider.login_required
     def put(self, subpath):
@@ -53,7 +90,7 @@ class S3Controller(MethodView):
             package.add_file(file.filename, file)
 
         try:
-            package.repo_annotations.append(RepoAnnotation(subpath, self.model.get_supported_repos()))
+            package.repo_annotations = self._generate_repo_annotations(subpath)
         except RuntimeError as err:
             logging.warning(str(err))
             return S3Controller.response_message(str(err), 400)
@@ -75,15 +112,16 @@ class S3Controller(MethodView):
 
     @auth_provider.login_required
     def post(self, subpath):
-        """Update metainformation of the repository."""
+        """Update metainformation of the repositories."""
         try:
-            repo_annotation = RepoAnnotation(subpath, self.model.get_supported_repos())
+            repo_annotations = self._generate_repo_annotations(subpath)
         except RuntimeError as err:
             logging.warning(str(err))
             return S3Controller.response_message(str(err), 400)
 
         try:
-            self.model.update_repo(repo_annotation)
+            for repo_annotation in repo_annotations:
+                self.model.update_repo(repo_annotation)
         except Exception as err:
             msg = "Can't update repository: " + str(err)
             logging.warning(msg)
